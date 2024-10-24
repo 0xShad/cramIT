9386f9d875e99e2a10ba72ea174bcbf583f3e0e5
shad <castroshadrack7@gmail.com>
2024-10-24 19:25:57 +0800

    added remove tab functionality

--- src/assets/components/Tabs.jsx
+++ src/assets/components/Tabs.jsx
@@ -11,12 +11,22 @@ export function Tabs(props) {
         } else {
             alert('invalid input or alrdy exist.')
         }
-       
+    }
+
+    const [isEditOn, setIsEditOn] = useState(false)
+
+    function editTab() {
+       setIsEditOn((prev) => !prev)
+    }
+
+    function removeTab(tabIndex) {
+        setTabs(tabs.filter((_, index) => index !== tabIndex))
+
     }
 
     return (
         <nav className="mt-4">
-            <div className="flex gap-3 flex-wrap">
+            <div className="flex gap-1.5 flex-wrap">
             {tabs.map((tabItem, tabIndex) => {
 
                 let numberOfTask = tabItem === 'All'
@@ -35,15 +45,21 @@ export function Tabs(props) {
                 }*/
 
                 return (
-                    <button key={tabIndex} className="tabButtons bg-[#3929ff] px-1.5 py-1 rounded-sm">
+                    <div className="relative p-1">
+                    <button key={tabIndex} className="tabButtons  bg-[#3929ff] px-1.5 py-1 rounded-sm">
+                        <i onClick={() => {removeTab(tabIndex)}} className={"cursor-pointer remove-tab-icon absolute top-1 right-0 fa-solid fa-x text-[10px] text-red-500 " + (isEditOn ? ' ' : 'hidden')}></i>
                         <h6 className="text-[10px]">{tabItem} <span className="text-[8px] text-gray-300">({numberOfTask})</span></h6>
                     </button>
+                    </div>
                 )
             })}
 
-            <button onClick={addTab} className="addTaskButton bg-white text-[#3929ff] font-bold p-1 rounded-md">
+            <div className="flex gap-2 p-2">
+            <button onClick={addTab} className="addTaskButton  bg-white text-[#3929ff] font-bold px-1.5 rounded-sm">
             <i className="fa-solid fa-plus"></i>
             </button>
+            <button onClick={editTab} className="text-[8px] bg-white text-[#3929ff] px-1.5 rounded-sm"> {isEditOn ? 'Done' : 'Edit'}</button>
+            </div>
             </div>
         </nav>
     )
